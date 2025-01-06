6d89d34cf599e485fa9c99a1cbfaa1b337280d20
vikash-one <vikash.1@freecharge.com>
2025-01-07 04:58:41 +0530

    Update backup_all_repos.sh

--- backup_all_repos.sh
+++ backup_all_repos.sh
@@ -2,7 +2,7 @@
 
 # Replace with your GitHub username and personal access token
 GITHUB_USERNAME="vikash-one"
-GITHUB_TOKEN="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"
+GITHUB_TOKEN="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" #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
 
 # Fetch all repository clone URLs using GitHub API
 repos=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/users/$GITHUB_USERNAME/repos?per_page=100 | jq -r '.[].clone_url')
